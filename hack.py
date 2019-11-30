6e2d62d84b9020df3d8740d42c0e81a5ebdb28f1
Coder Suresh <48799677+coderSuresh@users.noreply.github.com>
2019-11-30 10:34:46 +0545

    Create hack.py

---
+++ hack.py
@@ -0,0 +1,24 @@
+print("""
+  _|_|_|                  _|                        _|_|_|                                          _|
+_|          _|_|      _|_|_|    _|_|    _|  _|_|  _|        _|    _|  _|  _|_|    _|_|      _|_|_|  _|_|_|
+_|        _|    _|  _|    _|  _|_|_|_|  _|_|        _|_|    _|    _|  _|_|      _|_|_|_|  _|_|      _|    _|
+_|        _|    _|  _|    _|  _|        _|              _|  _|    _|  _|        _|            _|_|  _|    _|
+  _|_|_|    _|_|      _|_|_|    _|_|_|  _|        _|_|_|      _|_|_|  _|          _|_|_|  _|_|_|    _|    _|
+
+       @U7H0R:/>c0d3r 5ur35h   https://github.com/coderSuresh
+""")
+print("Disclaimer: Do not use for any illigal purpose. This tool is made for educational purpose only\n\n")
+print("1. Social Engineering\n2. Email Bombing\n3. Show Off\n4. Facebook Hacking")
+
+un = int(input("\n\nSelect a Number: "))
+
+if un == 1:
+    print("Social engineering is a very dangerous attack on which a attacker makes a victim fool and grabs confidential data")
+elif un == 2:
+    print("Email bombing is a process on which a attacker sends infinite number of email to the victim. This could be a very dangerous. ")
+elif un == 3:
+    print("LOL. This is for show off only. This is to very that I am a complete noob and wannabe hacker but I don't know anything yet")
+elif un == 4:
+    print("Facebook hacking is very very bad and very very hard. Don't even try to hack someone's facebook account.")
+else:
+    print("You should enter a number between 1 to 4")
